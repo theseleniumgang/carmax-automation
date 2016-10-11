bc0bf8cb5c7388da8b4a5d4b036bf8cfc4abb3ec
Ronak Gavandi <ronakgavandi@Ronaks-MacBook-Pro.local>
2016-10-10 21:31:17 -0400

    Create RequestInput Helper Class

---
+++ src/main/java/com/sqa/rg/helpers/InvalidAcceptableNumberException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: InvalidAcceptableNumberException.java<br>
+ *
+ *   Gavandi, Ronak<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Oct 10, 2016
+ *   
+ */
+
+package com.sqa.rg.helpers;
+
+/**
+ * InvalidAcceptableNumberException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Gavandi, Ronak
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class InvalidAcceptableNumberException extends Exception {
+
+}
